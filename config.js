29c5eac0cc624b525cfc950bf4d6835799531d9a
DevilScript <52322852+DevilScript@users.noreply.github.com>
2019-12-28 00:02:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'NjU3MjQ1Nzc3MzY5NzU5NzU1.XgYGpA.O1lJyMPkIs4m8-71bmWKTWOFwRQ';
 
-exports.PREFIX = 'm!';
+exports.PREFIX = '?';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyDmYJupJGeTaBRY1JtY_Ii7CIQiRj41TEo';
